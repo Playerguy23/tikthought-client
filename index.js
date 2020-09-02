2e4e6583338efe54b86840a771ab9285d3b3a34a
playerguy23 <maa1kivi@gmail.com>
2020-09-02 16:20:16 +0300

    Fixed view engine and added base of the home page.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/public'));
 app.use(express.static(__dirname + 'public'));
 app.use(morgan('combined'));
+
+const publicRote = require('./routes/publicRote');
+
+app.use('/', publicRote);
 
 app.listen(PORT, HOST, () => {
     console.log(`Server is running posrt ${PORT}. Server is hosted by ${HOST}.`)
